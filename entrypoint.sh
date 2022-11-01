baf1734ae7509074787710eeeec91ceceb81dfd2
arch <arch@local>
2022-11-01 14:49:00 +0100

    enable cloud init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/vyos/vyos-vm-images /vyos-vm-images
 pushd /vyos-vm-images
-ansible-playbook -e guest_agent=qemu -e enable_ssh=true qemu.yml
+ansible-playbook -e guest_agent=qemu -e enable_ssh=true -e cloud_init=true qemu.yml
 popd
 ls /tmp
 cp -fv /tmp/*.qcow2 /output
